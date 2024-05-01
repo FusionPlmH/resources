bab4dd72046fdde9e8c210be79f0772f48829ce1
Fusion <leemenhin@gmail.com>
2024-05-01 21:07:55 +0800

    Update cf+tailsclae-ufw-allow.sh

--- cf+tailsclae-ufw-allow.sh
+++ cf+tailsclae-ufw-allow.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
+echo ""
+echo ""
 echo "Proxmox VE Access only for Cloudflared Tunnel and Tailscale"
-echo "Welcome to use This Tool"
-echo "Powered by FsuionPlmH"
+echo ""
+echo ""
+echo ""
+echo "   Welcome to use This Tool "
+echo ""
+echo "    Powered by FsuionPlmH"
 
 if ! command -v ufw >/dev/null 2>&1; then
     if grep -Eqi "debian|ubuntu" /etc/issue* /proc/version* /etc/os-release*; then
         release=$(grep -Eo "debian|ubuntu" /etc/issue* /proc/version* /etc/os-release* | head -1)
         apt update && apt install -y ufw
     else
+        echo "==============="
         echo "Not supported"
+        echo "==============="
         exit 1
     fi
 fi
 
 for line in $(curl -s https://www.cloudflare.com/ips-v4); do
     echo "Reading $line from CloudFlare's official IP list."
-    ufw allow from $line to any port 80,443,8006
+    ufw allow from $line to any port 80
+    ufw allow from $line to any port 443
+    ufw allow from $line to any port 8006
 done
 
 for line in $(curl -s https://www.cloudflare.com/ips-v6); do
     echo "Reading $line from CloudFlare's official IP list."
-    ufw allow from $line to any port 80,443,8006
+    ufw allow from $line to any port 80
+    ufw allow from $line to any port 443
+    ufw allow from $line to any port 8006
 done
 
 ufw allow in on tailscale0
 ufw allow out on tailscale0
+y
 ufw enable
