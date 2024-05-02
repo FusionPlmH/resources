e90a459233eb9e150abe9c2ff47da0d248772df5
Fusion <leemenhin@gmail.com>
2024-05-02 12:26:37 +0800

    Update pve-security-access.sh

--- pve-security-access.sh
+++ pve-security-access.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
-
 echo ""
-echo ""
-echo "Proxmox VE Access only for Cloudflared Tunnel and Tailscale"
-echo ""
-echo ""
-echo ""
+echo "  Proxmox VE Access only for" 
+echo "Cloudflared Tunnel and Tailscale"
 echo "   Welcome to use This Tool "
-echo ""
 echo "    Powered by FsuionPlmH"
 
 if ! command -v ufw >/dev/null 2>&1; then
     if grep -Eqi "debian|ubuntu" /etc/issue* /proc/version* /etc/os-release*; then
         apt update && apt install -y ufw
     else
-        echo "==============="
         echo "Not supported"
-        echo "==============="
         exit 1
     fi
 fi
 
-for line in $(curl -s https://www.cloudflare.com/ips-v4); do
+for line in $(curl -s https://www.cloudflare.com/ips-v4)$(curl -s https://www.cloudflare.com/ips-v6); do
     ufw allow from $line to any port 443
 done
 
-for line in $(curl -s https://www.cloudflare.com/ips-v6); do
-    ufw allow from $line to any port 443
-done
-
-ufw allow in on tailscale0
-ufw allow out on tailscale0
+ufw allow in out on tailscale0
 ufw enable
